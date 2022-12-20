597ee64e77c479e6d42b24b598621c4a8d73f2bb
olegopro <evilgazz@gmail.com>
2022-12-20 22:28:21 +0300

    Добавил файлы в проект

---
+++ frontend-vue/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-tabs': 'off',
+    'indent': 'off',
+    'space-before-function-paren': 'off',
+    'no-trailing-spaces': 'off',
+    'vue/require-explicit-emits': 'error'
+  }
+
+}
