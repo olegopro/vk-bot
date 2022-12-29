23ecc0fc738686d4a7194bb3babfb35d20a5f799
olegopro <evilgazz@gmail.com>
2022-12-30 01:43:52 +0300

    Фикс для контейнеров vue и laravel

--- frontend-vue/vue.config.js
+++ frontend-vue/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: 'localhost',
+    // host: 'localhost',
     client: {
       webSocketURL: 'ws://0.0.0.0:3000/ws'
     }
