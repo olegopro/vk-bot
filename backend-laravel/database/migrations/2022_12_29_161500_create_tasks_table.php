0d2d55e72769810046d83fd8a8c4b2a7c5916fa3
olegopro <evilgazz@gmail.com>
2022-12-29 19:42:16 +0300

    Сделал миграцию для аккаунтов и задач

---
+++ backend-laravel/database/migrations/2022_12_29_161500_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('account_id')->unsigned();
+            $table->string('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
