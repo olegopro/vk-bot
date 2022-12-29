6fcb4591c75ecc54a639ef53b16f0440e8389217
olegopro <evilgazz@gmail.com>
2022-12-30 00:49:13 +0300

    Сделал hot reload для Vue через docker

--- frontend-vue/vue.config.js
+++ frontend-vue/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',
+    client: {
+      webSocketURL: 'ws://0.0.0.0:3000/ws'
+    }
+  }
 })
