93abd025a86227516d8862115f623d3d7633c31c
olegopro <evilgazz@gmail.com>
2023-01-22 20:31:18 +0300

    Подключил вк клиента

---
+++ backend-laravel/app/Http/Controllers/AccountController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Library\VkClient;
+use App\Models\Account;
+use App\Models\Task;
+use App\Services\VkService;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $accounts = Account::all();
+
+        return response($accounts);
+    }
+
+    /**
+     * Возвращает данные аккаунт по id задачи
+     *
+     * @param $taskId
+     * @return \Illuminate\Http\Response
+     */
+    public function accountByTaskId($taskId)
+    {
+        $account = Task::find($taskId)->account;
+
+        return response($account);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $account = Account::find($id);
+
+        return response($account);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getAccountData($id)
+    {
+        return (new VkClient())->request('users.get', [
+            'user_ids' => $id,
+            'fields'   => [
+                'photo_200',
+                'status',
+                'is_friend'
+            ]
+        ]);
+    }
+
+    public function getAccountFollowers($id, $limit = 10)
+    {
+        return (new VkClient())->request('users.getFollowers', [
+            'user_id' => $id,
+            'count'   => $limit,
+            'fields'  => [
+                'about',
+                'photo_200'
+            ]
+        ]);
+
+    }
+}
