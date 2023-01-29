9ccd805b805c5c04729ea469158f2397c0c4228d
olegopro <evilgazz@gmail.com>
2023-01-29 23:14:24 +0300

    Добавил проверку на ошибки при добавлении аккаунта

--- frontend-vue/src/components/Accounts/Modals/AddAccount.vue
+++ frontend-vue/src/components/Accounts/Modals/AddAccount.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="modal fade" id="addAccount" tabindex="-1" aria-labelledby="Add account" style="display: none;" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
-            <form @submit.prevent="addAccount(accessToken)" class="modal-content">
+            <form @submit.prevent="account" class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="Add account">Добавление аккаунта</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -19,10 +19,13 @@
                         >
 
                     </div>
+                    <div v-if="errorMessage" class="alert alert-danger" role="alert">
+                        {{ errorMessage }}
+                    </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
-                    <button type="submit" class="btn btn-success" data-bs-dismiss="modal">Добавить</button>
+                    <button type="button" class="btn btn-secondary" @click="modalHide">Отмена</button>
+                    <button type="submit" class="btn btn-success">Добавить</button>
                 </div>
             </form>
         </div>
@@ -31,16 +34,39 @@
 
 <script>
     import { mapActions } from 'vuex'
+    import { Modal } from 'bootstrap'
 
     export default {
         data() {
             return {
-                accessToken: null
+                accessToken: null,
+                modal: null,
+                showError: false,
+                errorMessage: null
             }
         },
 
+        mounted() {
+            this.modal = new Modal(document.getElementById('addAccount'))
+        },
+
         methods: {
-            ...mapActions('account', ['addAccount'])
+            ...mapActions('account', ['addAccount']),
+
+            account() {
+                this.addAccount(this.accessToken)
+                    .then(() => {
+                        this.modal.hide()
+                    })
+                    .catch(error => {
+                        this.errorMessage = error.response.data.message
+                        console.log(error.response.data.message)
+                    })
+            },
+
+            modalHide() {
+                this.modal.hide()
+            }
         }
     }
 </script>
