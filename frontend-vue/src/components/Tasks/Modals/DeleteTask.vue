61a667f2fc3f70017d668bf4a3fdc94914155760
olegopro <evilgazz@gmail.com>
2023-02-03 18:48:09 +0300

    Добавил обработку кнопки удалить задачу

--- frontend-vue/src/components/Tasks/Modals/DeleteTask.vue
+++ frontend-vue/src/components/Tasks/Modals/DeleteTask.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="modal fade" id="deleteTask" tabindex="-1" aria-labelledby="Delete task" style="display: none;" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
-            <form @submit.prevent class="modal-content">
+            <form @submit.prevent="deleteTaskById" class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="Delete task"></h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <p>Удалить задачу c id <strong>{{ taskId }}</strong></p>
+                    <p>Удалить задачу c ID <strong>{{ taskId }}</strong></p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
-                    <button type="submit" @click="deleteTask(taskId)" class="btn btn-danger">Удалить</button>
+                    <button type="button" class="btn btn-secondary" @click="modalHide">Отмена</button>
+                    <button type="submit" class="btn btn-danger">Удалить</button>
                 </div>
             </form>
         </div>
@@ -20,12 +20,34 @@
 
 <script>
     import { mapActions } from 'vuex'
+    import { Modal } from 'bootstrap'
 
     export default {
         props: ['taskId'],
 
+        data() {
+            return {
+                modal: null
+            }
+        },
+
+        mounted() {
+            this.modal = new Modal(document.getElementById('deleteTask'))
+        },
+
         methods: {
-            ...mapActions('tasks', ['deleteTask'])
+            ...mapActions('tasks', ['deleteTask']),
+
+            deleteTaskById() {
+                this.deleteTask(this.taskId)
+                    .then(() => {
+                        this.modal.hide()
+                    })
+            },
+
+            modalHide() {
+                this.modal.hide()
+            }
         }
     }
 </script>
--- frontend-vue/src/store/modules/tasks.js
+++ frontend-vue/src/store/modules/tasks.js
@@ -15,7 +15,11 @@ export default {
         },
 
         deleteTask(state, id) {
-            state.tasks(id, 1)
+            // Вариант #1
+            // state.tasks = state.tasks.filter(task => task.id !== id)
+
+            // Вариант #2
+            state.tasks.splice(state.tasks.findIndex(key => key.id === id), 1)
         }
     },
 
@@ -31,7 +35,7 @@ export default {
 
         async deleteTask({ commit }, id) {
             await axios.delete((`http://localhost:8080/api/tasks/${id}`))
-            // commit('deleteTask', id)
+            commit('deleteTask', id)
         }
     },
 
