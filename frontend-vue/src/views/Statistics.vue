fe7995cf036dddb55f9cc56b5d9c549e463af5c9
olegopro <evilgazz@gmail.com>
2023-02-06 19:56:09 +0300

    Добавил chart.js

--- frontend-vue/src/views/Statistics.vue
+++ frontend-vue/src/views/Statistics.vue
@@ -6,47 +6,41 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Аккаунт</th>
-                    <th scope="col">Статус</th>
-                    <th scope="col">Действия</th>
-                    <th scope="col">Добавлен</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row">id задачи</th>
-                    <td>логин</td>
-                    <td>
-                        <TaskStatus :type="'active'" />
-                    </td>
-                    <td>
-                        <router-link custom to="/" v-slot="{navigate}">
-                            <a class="btn btn-primary me-2" @click="navigate">
-                                <svg width="16" height="20">
-                                    <use xlink:href="#info"></use>
-                                </svg>
-                            </a>
-                        </router-link>
-
-                    </td>
-                    <td>20.10.2022</td>
-
-                </tr>
-                </tbody>
-            </table>
+            <Bar
+                id="my-chart-id"
+                :options="chartOptions"
+                :data="chartData"
+            />
         </div>
     </div>
 </template>
 
 <script>
-    import TaskStatus from '../components/Tasks/TaskStatus.vue'
+    import { Bar } from 'vue-chartjs'
+    import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+    ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
     export default {
-        components: { TaskStatus }
+        name: 'BarChart',
+        components: { Bar },
+        data() {
+            return {
+                chartData: {
+                    labels: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+                    datasets: [{ data: [40, 20, 12, 64, 42, 22, 74] }]
+                },
+                chartOptions: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false
+                            // position: 'bottom'
+                        }
+                    }
+                }
+            }
+        }
     }
 </script>
 
