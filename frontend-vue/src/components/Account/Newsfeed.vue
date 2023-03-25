97916750e41182c0cdfd6d89c63b8757e8fd88e6
olegopro <evilgazz@gmail.com>
2023-03-25 12:24:58 +0300

    Сделал индикатор загрузки лайка

--- frontend-vue/src/components/Account/Newsfeed.vue
+++ frontend-vue/src/components/Account/Newsfeed.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row mt-5 mb-5" v-masonry transition-duration="0s" item-selector=".item">
         <!--<h1>Публикации в ленте</h1>-->
-        <div class="col-3 mb-3 item" v-masonry-tile v-for="post in newsfeed" :key="post.source_id">
+        <div class="col-3 mb-3 item" v-masonry-tile v-for="(post, index) in newsfeed" :key="post.source_id">
             <div class="card">
                 <img class="bd-placeholder-img card-img-top"
                      width="100%"
@@ -13,10 +13,11 @@
                         type="button"
                         :id="post.post_id"
                         :disabled="post.likes.user_likes === 1"
-                        @click="addLikeToPost(post.owner_id, post.post_id)"
+                        @click="addLikeToPost(post.owner_id, post.post_id, index)"
                         ref="buttons"
                 >
                     Лайкнуть
+                    <span v-show="loadingStatus[index]" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                 </button>
             </div>
         </div>
@@ -29,7 +30,8 @@
     export default {
         data() {
             return {
-                userID: null
+                userID: null,
+                loadingStatus: []
             }
         },
 
@@ -47,20 +49,25 @@
 
         mounted() {
             this.accountNewsfeed(this.userID)
+            this.loadingStatus = new Array(this.newsfeed.length).fill(false)
         },
 
         methods: {
             ...mapActions('account', ['accountNewsfeed', 'addLike']),
 
-            async addLikeToPost(ownerId, itemId) {
+            async addLikeToPost(ownerId, itemId, index) {
                 this.disableButton(itemId)
 
                 const payload = { accountId: this.userID, ownerId, itemId }
+                this.loadingStatus[index] = true
+
                 await this.addLike(payload)
                     .then(() => {
                         const button = this.$refs.buttons.find(item => +item.id === itemId)
                         button.classList.remove('btn-danger')
                         button.classList.add('btn-success')
+                    }).finally(() => {
+                        this.loadingStatus[index] = false
                     })
             },
 
