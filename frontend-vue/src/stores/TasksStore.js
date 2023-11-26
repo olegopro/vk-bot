93d133c1e265ad64559eef1365ae6c3699d5ad4a
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2023-11-26 19:54:41 +0300

    Сделал рефактор TaskStore
    Добавил then после получения ответов от axios

--- frontend-vue/src/stores/TasksStore.js
+++ frontend-vue/src/stores/TasksStore.js
@@ -13,35 +13,29 @@ export const useTasksStore = defineStore('tasks', {
             this.tasks = data
         },
 
-        async accountByTaskId(id) {
-            return await axios.post(`http://localhost:8080/api/account/task/${id}`)
-        },
-
-         async taskDetails(taskId) {
+        async taskDetails(taskId) {
             const response = await axios.post(`http://localhost:8080/api/tasks/task-info/${taskId}`)
             return response.data
         },
 
         async deleteLike(taskId) {
             await axios.delete(`http://localhost:8080/api/tasks/delete-like/${taskId}`)
-            // После удаления лайка обновляем данные о задаче
-            this.taskDetails(taskId)
+                .then(() => this.taskDetails(taskId))
         },
 
         async deleteTask(id) {
-            console.log('deleteTask -id', id)
             await axios.delete(`http://localhost:8080/api/tasks/delete-task-by-id/${id}`)
-            this.tasks = this.tasks.filter(task => task.id !== id)
+                .then(() => this.tasks = this.tasks.filter(task => task.id !== id))
         },
 
         async deleteSingleTaskById(id) {
             await axios.delete(`http://localhost:8080/api/tasks/deleteTask/${id}`)
-            this.tasks = this.tasks.filter(task => task.id !== id)
+                .then(() => this.tasks = this.tasks.filter(task => task.id !== id))
         },
 
         async deleteAllTasks() {
             await axios.delete('http://localhost:8080/api/tasks/delete-all-tasks')
-            this.tasks = []
+                .then(() => this.tasks = [])
         }
     },
 
