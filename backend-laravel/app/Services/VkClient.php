6181bee20c24a7fdcbb4c32da40fdfcec0056147
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2023-12-28 21:20:20 +0300

    Убрал лишний код из сервиса VkClient

--- backend-laravel/app/Services/VkClient.php
+++ backend-laravel/app/Services/VkClient.php
@@ -8,9 +8,8 @@ use Illuminate\Support\Facades\Log;
 class VkClient
 {
     private $api;
-    private $account_access_token;
 
-    public function __construct($account_access_token = null)
+    public function __construct()
     {
 	    $this->api = new Client(config('services.vk.version'));
 	    $this->api->setDefaultToken(config('services.vk.token'));
