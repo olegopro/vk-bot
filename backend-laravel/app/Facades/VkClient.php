fb09c197bae39eff0c9095cae4cde84fb5692c48
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2024-01-08 21:31:55 +0300

    Улучшил фасад VkClient.php
    - Удалены неиспользуемые методы
    - Добавлена ссылка на класс VkClientService

--- backend-laravel/app/Facades/VkClient.php
+++ backend-laravel/app/Facades/VkClient.php
@@ -5,18 +5,7 @@ namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static mixed request(string $method, array $parameters = [], string|null $token = null)
- * @method static array getAccountData(mixed $ids)
- * @method static array getGroupData(mixed $id)a
- * @method static array getAccountFollowers(mixed $id, int $limit)
- * @method static array getAccountFriends(mixed $id, int $limit)
- * @method static array getAccountCountFriends(string $accountId, string $ownerId, string $token)
- * @method static array getAccountInfo(string $token)
- * @method static array setAccountData(string $token, $accountRepository)
- * @method static array getAccountNewsfeed(int $accountId, ?string $startFrom, $loggingService)
- * @method static array addLike(int $ownerId, int $itemId, string $accessToken, $loggingService)
- * @method static string getAccessTokenByAccountID(int $account_id)
- * @method static string getScreenNameByToken(string $access_token)
+ * @mixin \App\Services\VkClientService
  */
 class VkClient extends Facade
 {
