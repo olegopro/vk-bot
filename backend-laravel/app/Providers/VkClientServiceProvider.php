e67b870166d5d4d32429dc69c1b212400e9147ab
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2024-02-20 20:17:30 +0300

    Добавлено описание для VkClientServiceProvider

--- backend-laravel/app/Providers/VkClientServiceProvider.php
+++ backend-laravel/app/Providers/VkClientServiceProvider.php
@@ -7,8 +7,20 @@ use App\Repositories\AccountRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Services\VkClientService;
 
+/**
+ * Сервис-провайдер для VkClientService.
+ *
+ * Этот класс регистрирует VkClientService в контейнере служб Laravel,
+ * позволяя легко внедрять зависимости и использовать сервис в приложении.
+ */
 class VkClientServiceProvider extends ServiceProvider
 {
+    /**
+     * Регистрация сервиса в контейнере приложения.
+     *
+     * Этот метод регистрирует VkClientService как singleton в контейнере служб,
+     * обеспечивая один и тот же экземпляр сервиса на протяжении всего запроса.
+     */
     public function register()
     {
         $this->app->singleton('VkClientService', function ($app) {
@@ -16,6 +28,12 @@ class VkClientServiceProvider extends ServiceProvider
         });
     }
 
+    /**
+     * Загрузка любых сервисов, требуемых провайдером.
+     *
+     * Этот метод создает псевдоним 'VkClientService' для фасада VkClient,
+     * упрощая доступ к сервису через фасад.
+     */
 	public function boot()
 	{
 		$this->app->alias('VkClientService', VkClient::class);
