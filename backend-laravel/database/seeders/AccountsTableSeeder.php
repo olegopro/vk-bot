390cbce8297982ab0b89ec48633d586bea5ef3b2
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2024-03-06 17:59:39 +0300

    Добавил seeder для таблицы Accounts

---
+++ backend-laravel/database/seeders/AccountsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class AccountsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 50) as $index) {
+            DB::table('accounts')->insert([
+                'account_id'   => $faker->unique()->randomNumber(5),
+                'access_token' => $faker->sha256,
+                'screen_name'  => $faker->userName,
+                'first_name'   => $faker->firstName,
+                'last_name'    => $faker->lastName,
+                'bdate'        => $faker->date($format = 'Y-m-d', $max = 'now'),
+            ]);
+        }
+    }
+}
