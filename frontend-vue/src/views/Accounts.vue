198bd0fe9570dae08c144ed546514684919a9845
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2024-03-06 19:16:32 +0300

    Улучшил стилизацию и функционал таблиц
    - Добавлены CSS переменные для теней и радиуса границ в _main-overflow.scss.
    - Использование CSS переменных для радиуса границ таблицы и теней.
    - Удалены старые стили тени и радиуса границ из стилей таблицы, заменены на CSS переменные.
    - Добавлены стили для позиционирования заголовков таблицы в виде sticky.
    - Введен новый компонент PerfectScrollbar для обертки таблицы в Accounts.vue.

--- frontend-vue/src/views/Accounts.vue
+++ frontend-vue/src/views/Accounts.vue
@@ -16,49 +16,51 @@
 
     <div class="row">
         <div class="col-12">
-            <table class="table table-hover" v-if="accountsStore.accounts.length">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Имя и фамилия</th>
-                        <th scope="col">Логин</th>
-                        <th scope="col">Действия</th>
-                        <th scope="col">День рождения</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="account in accountsStore.accounts" :key="account.account_id">
-                        <th scope="row">{{ account.account_id }}</th>
-                        <td>{{ account.first_name }} {{ account.last_name }}</td>
-                        <td>{{ account.screen_name }}</td>
+            <PerfectScrollbar>
+                <table class="table table-hover" v-if="accountsStore.accounts.length">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Имя и фамилия</th>
+                            <th scope="col">Логин</th>
+                            <th scope="col">Действия</th>
+                            <th scope="col">День рождения</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="account in accountsStore.accounts" :key="account.account_id">
+                            <th scope="row">{{ account.account_id }}</th>
+                            <td>{{ account.first_name }} {{ account.last_name }}</td>
+                            <td>{{ account.screen_name }}</td>
 
-                        <td>
-                            <router-link custom :to="{name: 'Account', params: {id: account.account_id}}" v-slot="{navigate}">
-                                <a class="btn btn-primary me-2 button-style" @click="navigate">
+                            <td>
+                                <router-link custom :to="{name: 'Account', params: {id: account.account_id}}" v-slot="{navigate}">
+                                    <a class="btn btn-primary me-2 button-style" @click="navigate">
+                                        <svg width="16" height="16">
+                                            <use xlink:href="#info"></use>
+                                        </svg>
+                                    </a>
+                                </router-link>
+
+                                <button
+                                    class="btn btn-danger button-style"
+                                    data-bs-target="#deleteAccount"
+                                    data-bs-toggle="modal"
+                                    type="button"
+                                    @click="getLogin(account.screen_name, account.account_id)"
+                                >
                                     <svg width="16" height="16">
-                                        <use xlink:href="#info"></use>
+                                        <use xlink:href="#delete"></use>
                                     </svg>
-                                </a>
-                            </router-link>
+                                </button>
+                            </td>
 
-                            <button
-                                class="btn btn-danger button-style"
-                                data-bs-target="#deleteAccount"
-                                data-bs-toggle="modal"
-                                type="button"
-                                @click="getLogin(account.screen_name, account.account_id)"
-                            >
-                                <svg width="16" height="16">
-                                    <use xlink:href="#delete"></use>
-                                </svg>
-                            </button>
-                        </td>
-
-                        <td>{{ account.bdate }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <h3 v-else class="text-center">Список аккаунтов пустой</h3>
+                            <td>{{ account.bdate }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h3 v-else class="text-center">Список аккаунтов пустой</h3>
+            </PerfectScrollbar>
         </div>
     </div>
 
@@ -84,3 +86,11 @@
 
     onMounted(() => accountsStore.fetchAccounts())
 </script>
+
+<style lang="scss" scoped>
+    .ps {
+        height: 80vh;
+        box-shadow: var(--ps-shadow-box);
+        border-radius: var(--ps-border-radius);
+    }
+</style>
