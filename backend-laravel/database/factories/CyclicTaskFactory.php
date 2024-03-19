1443e84186684d395c446c93e347d89dfe1bcf5a
Oleg Desyatnikov <evilgazz@gmail.com>
2024-03-19 17:22:30 +0300

    Расширил фабрику циклических задач
    - Добавлен подробный алгоритм генерации распределения лайков
    - Реализована генерация случайных времён выполнения задач по дням недели
    - Оптимизированы исходные данные для поля selected_times
    - Улучшен алгоритм формирования начальной даты задачи
    - Ограничен список возможных статусов задач

--- backend-laravel/database/factories/CyclicTaskFactory.php
+++ backend-laravel/database/factories/CyclicTaskFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
-use App\Models\CyclicTask; // Убедитесь, что этот путь соответствует вашей реальной модели
+use App\Models\CyclicTask;
+
+// Убедитесь, что этот путь соответствует вашей реальной модели
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
@@ -12,15 +14,39 @@ class CyclicTaskFactory extends Factory
 
     public function definition()
     {
+        // Генерация likes_distribution
+        $likesDistribution = [];
+        $minutesCount = $this->faker->numberBetween(1, 60); // Сколько минут будет в массиве
+        for ($i = 0; $i < $minutesCount; $i++) {
+            $likesDistribution[] = $this->faker->unique()->numberBetween(1, 60);
+        }
+
+        sort($likesDistribution); // Сортировка чисел от меньшего к большему
+        $this->faker->unique($reset = true); // Сброс уникальности, чтобы избежать проблем при повторных вызовах
+
+        // Генерация selected_times
+        $daysOfWeek = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+        $selectedTimes = [];
+
+        foreach ($daysOfWeek as $day) {
+            $hours = array_fill(0, 24, false); // Инициализация массива из 24 элементов значениями false
+
+            for ($i = 0; $i < 7; $i++) { // Допустим, случайно выбираем 7 часов в день как true
+                $hours[$this->faker->numberBetween(0, 23)] = true;
+            }
+
+            $selectedTimes[$day] = $hours;
+        }
+
         return [
-            'account_id' => $this->faker->numberBetween(1, 1000), // Предполагая, что у вас есть аккаунты с ID от 1 до 1000
-            'total_task_count' => $this->faker->numberBetween(1, 100),
+            'account_id'            => $this->faker->numberBetween(1, 1000),
+            'total_task_count'      => $this->faker->numberBetween(1, 100),
             'remaining_tasks_count' => $this->faker->numberBetween(1, 100),
-            'tasks_per_hour' => $this->faker->numberBetween(1, 24),
-            'likes_distribution' => $this->faker->randomElement(['uniform', 'random', 'peak', null]),
-            'selected_times' => json_encode([$this->faker->time($format = 'H:i:s'), $this->faker->time($format = 'H:i:s')]),
-            'status' => $this->faker->randomElement(['active', 'failed', 'canceled', 'done', 'pending', 'queued', 'pause']),
-            'started_at' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'tasks_per_hour'        => $this->faker->numberBetween(1, 24),
+            'likes_distribution'    => json_encode($likesDistribution),
+            'selected_times'        => $selectedTimes,
+            'status'                => $this->faker->randomElement(['active', 'done', 'pause']),
+            'started_at'            => $this->faker->dateTimeBetween('-1 month', 'now'),
         ];
     }
 }
