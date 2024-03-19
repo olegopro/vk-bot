5deaf4b0d83f8eabe1459f26578e71b5965aaed0
Oleg Desyatnikov <evilgazz@gmail.com>
2024-03-19 17:15:44 +0300

    Оптимизировал модальное окно деталей задачи
    - Убраны скроллбары в модальном окне деталей задачи
    - Добавлена поддержка PerfectScrollbar для лучшей прокрутки
    - Исправлены стили заголовков и границ в аккордеоне

--- frontend-vue/src/components/Tasks/Modals/TaskDetails.vue
+++ frontend-vue/src/components/Tasks/Modals/TaskDetails.vue
@@ -1,54 +1,58 @@
 <template>
     <div class="modal fade" id="taskDetailsModal" tabindex="-1" aria-labelledby="Task details" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header" v-if="tasksStore.taskDetails">
                     <h1 class="modal-title fs-5" id="Delete task">Информация о задаче #{{ taskData.taskId }}</h1>
-                    <!--<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>-->
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
-                <div class="modal-body py-0">
-                    <div class="d-flex mb-3" v-if="tasksStore.taskDetails">
-                        <img :src="tasksStore.taskDetails.attachments[0].photo.sizes[2].url" class="rounded-1" alt="">
+                <PerfectScrollbar class="ps-modal">
+                    <div class="modal-body py-0">
+                        <div class="d-flex mb-3" v-if="tasksStore.taskDetails">
+                            <img :src="tasksStore.taskDetails.attachments[0].photo.sizes[2].url" class="rounded-1 w-100" alt="">
+                        </div>
+
                         <div class="ps-3 d-flex flex-column">
                             <p class="mb-1 w-100">Количество лайков: <b>{{ tasksStore.taskDetails.likes.count }}</b></p>
                             <p>Дата публикации: <b>{{ formatData(tasksStore.taskDetails.attachments[0].photo.date) }}</b></p>
                         </div>
-                    </div>
 
-                    <div class="accordion" v-if="tasksStore.taskDetails">
-                        <div class="accordion-item">
+                        <div class="accordion" v-if="tasksStore.taskDetails">
+                            <div class="accordion-item">
 
-                            <h2 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#listOfLikes" aria-expanded="false" aria-controls="listOfLikes">
-                                    Список лайкнувших
-                                </button>
-                            </h2>
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#listOfLikes" aria-expanded="false" aria-controls="listOfLikes">
+                                        Список лайкнувших
+                                    </button>
+                                </h2>
 
-                            <div id="listOfLikes" class="accordion-collapse collapse">
-                                <div class="accordion-body p-0">
-                                    <div class="accordion">
-                                        <div class="accordion-item" v-for="user in tasksStore.taskDetails.liked_users" :key="user.id">
-                                            <h2 class="accordion-header">
-                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#id' + user.id " aria-expanded="false" :aria-controls="user.id">
-                                                    {{ user.first_name }} {{ user.last_name }}
-                                                </button>
-                                            </h2>
-                                            <div :id="'id' + user.id" class="accordion-collapse collapse">
-                                                <div class="accordion-body">
-                                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-                                                    <code>.accordion-body</code>, though the transition does limit overflow.
+                                <div id="listOfLikes" class="accordion-collapse collapse">
+                                    <div class="accordion-body p-0">
+                                        <div class="accordion">
+                                            <div class="accordion-item" v-for="user in tasksStore.taskDetails.liked_users" :key="user.id">
+                                                <h2 class="accordion-header">
+                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#id' + user.id " aria-expanded="false" :aria-controls="user.id">
+                                                        {{ user.first_name }} {{ user.last_name }}
+                                                    </button>
+                                                </h2>
+                                                <div :id="'id' + user.id" class="accordion-collapse collapse">
+                                                    <div class="accordion-body">
+                                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+                                                        <code>.accordion-body</code>, though the transition does limit overflow.
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
-
                         </div>
-                    </div>
 
-                </div>
+                    </div>
+                </PerfectScrollbar>
+
                 <div class="modal-footer">
                     <button type="button"
                             class="btn btn-danger"
@@ -66,7 +70,7 @@
 </template>
 
 <script setup>
-    import { ref, computed, watch, defineProps, inject, onMounted, onUnmounted } from 'vue'
+    import { ref, computed, defineProps, inject, onMounted, onUnmounted } from 'vue'
     import { format } from 'date-fns'
     import { useTasksStore } from '@/stores/TasksStore'
     import { showErrorNotification } from '@/helpers/notyfHelper'
@@ -125,8 +129,7 @@
                     #listOfLikes {
                         .accordion {
                             .accordion-item {
-                                border-left: 0;
-                                border-right: 0;
+                                border-radius: 0 !important;
 
                                 &:first-child {
                                     border-radius: 0;
@@ -142,6 +145,8 @@
                                 }
 
                                 .accordion-header {
+                                    border-radius: 0;
+
                                     button {
                                         border-radius: 0;
                                     }
