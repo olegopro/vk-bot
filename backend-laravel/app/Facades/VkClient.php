b2ea68072ba751bb701a6d9025ad6fb41bdf1e10
olegopro <evilgazz@gmail.com>
2025-03-25 20:22:00 +0300

    Добавил PHP-документацию для контроллеров и фасадов
        - Описал фасад VkClient, указав его назначение и методы взаимодействия с API ВКонтакте.
        - Добавил PHP-докблоки для SettingsController с описанием методов getSettings и saveSettings.
        - Добавил PHP-докблоки для StatisticController с описанием методов getStatistic и getWeeklyTaskStats.

--- backend-laravel/app/Facades/VkClient.php
+++ backend-laravel/app/Facades/VkClient.php
@@ -5,8 +5,16 @@ namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ *  Фасад для взаимодействия с API ВКонтакте.
+ *
+ *  Предоставляет удобный интерфейс для доступа к методам сервиса VkClientService.
+ *  Используется для выполнения запросов к API ВКонтакте, получения данных аккаунтов,
+ *  управления задачами лайков и другими операциями, связанными с ВКонтакте.
+ *
  * @mixin \App\Services\VkClientService
  */
+
+
 class VkClient extends Facade
 {
     protected static function getFacadeAccessor()
--- backend-laravel/app/Http/Controllers/SettingsController.php
+++ backend-laravel/app/Http/Controllers/SettingsController.php
@@ -5,8 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Settings;
 use Illuminate\Http\Request;
 
+/**
+ * Контроллер для управления настройками приложения.
+ *
+ * Предоставляет методы для получения и сохранения глобальных настроек
+ * системы, таких как отображение подписчиков, друзей и таймаут задач.
+ */
 final class SettingsController extends Controller
 {
+    /**
+     * Получает все настройки приложения.
+     *
+     * Этот метод извлекает все записи из таблицы settings
+     * и возвращает их в ответе HTTP запроса.
+     *
+     * @return \Illuminate\Http\Response Ответ, содержащий все настройки приложения
+     */
     public function getSettings()
     {
         $settings = Settings::all();
@@ -14,6 +28,18 @@ final class SettingsController extends Controller
         return response($settings);
     }
 
+    /**
+     * Сохраняет настройки приложения.
+     *
+     * Этот метод обновляет значения настроек в базе данных на основе
+     * данных, полученных в HTTP запросе. Обновляются следующие настройки:
+     * - show_followers: отображение подписчиков
+     * - show_friends: отображение друзей
+     * - task_timeout: таймаут задач
+     *
+     * @param \Illuminate\Http\Request $request HTTP запрос, содержащий новые значения настроек
+     * @return void
+     */
     public function saveSettings(Request $request)
     {
         Settings::find(1)->update([
