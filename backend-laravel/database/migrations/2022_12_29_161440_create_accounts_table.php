962c6a0ede4ad4217d685e265e26065c9b345553
olegopro <evilgazz@gmail.com>
2025-08-20 00:24:53 +0300

    Изменил тип данных для столбца access_token в accounts
    - Заменил тип данных столбца access_token с string на text в миграции
    - Обеспечил поддержку более длинных строк токенов для хранения данных

--- backend-laravel/database/migrations/2022_12_29_161440_create_accounts_table.php
+++ backend-laravel/database/migrations/2022_12_29_161440_create_accounts_table.php
@@ -15,7 +15,7 @@ class CreateAccountsTable extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->integer('account_id')->unsigned()->unique();
-            $table->string('access_token');
+            $table->text('access_token');
             $table->string('screen_name');
             $table->string('first_name');
             $table->string('last_name');
